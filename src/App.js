b8983d8b413a0018dd2e5a71bce6a1fb92143136
simran <bhalla.simran297@gmail.com>
2021-11-11 19:54:09 +0530

    set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <h2>hwelldsf</h2>
       </header>
     </div>
   );
